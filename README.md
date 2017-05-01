8c5576b09ca20fdbfbc1f7e179dd1d871c2768f6
Frederic <fred.theriault@gmail.com>
2017-05-01 09:44:50 -0400

    Added first version of A* demo

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # A-Star
 JavaScript A* implementation
+
+## Goal
+
+Small utility file to help sprites find their way in a map.
+
+## How it works
+
+When using the file a-star.js, the parameters are:
+
+currentX, currentY : current position of sprite  
+destinationX, destinationY : destination point  
+map : must have the function getWalkableCost(x, y), which must return -1 if a wall, or the cost of passing through that path (10 should be default, 15 could be high ground and 20 could be a sand pit, for example)  
+minObstacleSize : in order to jump to next node, how many pixels can we jump over? 1 is ideal, but requires a lot more processing/memory  
+isSupportDiagonal : can the sprite move diagonally?  
+isRoundDiagonalEdge : is it possible for the sprite to pass near a wall in diagonal? (if true, less cpu intensive, but more error prone). If set to false, a collision detection logic could fail if a sprite attempts to pass through a wall diagonally  
+					  
+## About the demo
+
+You can click on the map to change the end point.
+
+
---
+++ js/a-star.js
@@ -0,0 +1,129 @@
+// =========================================
+// Author : Frederic Theriault
+// URL    : http://www.frederictheriault.com
+// -----------------------------------------
+
+/**
+ * Calculates a path array using the A* algorithm
+ *
+ * currentX/Y : current position of sprite
+ * destinationYX : destination point
+ * map : must have the function getWalkableCost(x, y), which must return -1 if a wall, or the cost of passing through that path 
+ *		 (10 should be default, 15 could be high ground and 20 could be a sand pit, for example) 
+ * minObstacleSize : in order to jump to next node, how many pixels can we jump over? 1 is ideal, but requires a lot more processing/memory
+ * isRoundDiagonalEdge : is it possible for the sprite to pass near a wall in diagonal? (if true, less cpu intensive, but more error prone). 
+ *						 If set to false, a collision detection logic could fail if a sprite attempts to pass through a wall diagonally
+ * 
+ * returns the path of points to follow
+ */ 
+function calculatePath (currentX, currentY, destinationX, destinationY, map, minObstacleSize, isSupportDiagonal, isRoundDiagonalEdge) {	
+	var Node = function (x, y, cost, parent) {
+		this.x = x;
+		this.y = y;
+		this.parent = parent; 
+		
+		this.cost = cost;	// g
+		this.h = (Math.abs(this.x - destinationX) + Math.abs(this.y - destinationY)) * 10;	// Heuristic (Manhattan method) cost to reach dest.
+
+		this.score = this.cost + (this.parent != null ? this.parent.score : 0 ) + this.h;
+	}
+
+	var path = [];
+	var openList = [];
+	var closedList = [];
+	var resultNode = null;
+	var currentNode;
+	var x, y;
+	var cost = 0;
+	var inListNode = null;
+	var node = null;
+
+	openList.push(new Node(currentX, currentY, 0, null));
+
+	// while exist not found or there are nodes to visit
+	while (openList.length > 0) {
+		currentNodeIdx = 0;
+		currentNode = openList[currentNodeIdx];
+		
+		for (var j = 0; j < openList.length; j++) {
+			if (currentNode.score > openList[j].score) {
+				currentNode = openList[j];
+				currentNodeIdx = j;
+			}
+		}
+
+		openList.splice(currentNodeIdx, 1);
+		closedList[currentNode.x + "-" + currentNode.y] = 1;
+
+		// for all adjacent node
+		for (var i = 0; i < (isSupportDiagonal ? 8 : 4); i++) {
+			diagAccepted = true;
+
+			if (i == 0) {x = 0; y = -1 * minObstacleSize}
+			else if (i == 1) {x = 1 * minObstacleSize; y = 0;}
+			else if (i == 2) {x = 0; y = 1 * minObstacleSize;}
+			else if (i == 3) {x = -1 * minObstacleSize; y = 0;}
+			else if (i == 4) {x = -1 * minObstacleSize; y = -1  * minObstacleSize;}
+			else if (i == 5) {x = 1 * minObstacleSize; y = -1  * minObstacleSize;}
+			else if (i == 6) {x = -1 * minObstacleSize; y =  1 * minObstacleSize;}
+			else if (i == 7) {x = 1 * minObstacleSize; y =  1 * minObstacleSize;}
+
+			if (i > 3 && isSupportDiagonal && !isRoundDiagonalEdge) {
+				if ((i == 4 && (map.getWalkableCost(currentNode.x - 1 * minObstacleSize, currentNode.y) == -1 || (map.getWalkableCost(currentNode.x, currentNode.y - 1 * minObstacleSize) == -1))) ||
+					(i == 5 && (map.getWalkableCost(currentNode.x + 1 * minObstacleSize, currentNode.y) == -1 || (map.getWalkableCost(currentNode.x, currentNode.y - 1 * minObstacleSize) == -1))) ||
+					(i == 6 && (map.getWalkableCost(currentNode.x - 1 * minObstacleSize, currentNode.y) == -1 || (map.getWalkableCost(currentNode.x, currentNode.y + 1 * minObstacleSize) == -1))) ||
+					(i == 7 && (map.getWalkableCost(currentNode.x + 1 * minObstacleSize, currentNode.y) == -1 || (map.getWalkableCost(currentNode.x, currentNode.y + 1 * minObstacleSize) == -1)))) {
+					diagAccepted = false;
+				} 
+			}
+
+			// if the node isn't an objstacle or part of closed list
+			cost = map.getWalkableCost(currentNode.x + x, currentNode.y + y);
+			if (cost != -1 && diagAccepted &&
+				closedList[(currentNode.x + x) + "-" + (currentNode.y + y)] == null) {
+				node = new Node(currentNode.x + x, currentNode.y + y, cost + (i > 3 ? 4 : 0), currentNode);	
+				inListNode = null;
+
+				for (var j = 0; j < openList.length; j++) {
+					if (openList[j].x == node.x && openList[j].y == node.y) {
+						inListNode = openList[j];
+						break;
+					}
+				}
+
+				if (inListNode == null) {
+					openList.push(node);
+				}
+				else {
+					if (node.score < inListNode.score) {
+						inListNode = node;
+					}
+				}
+
+				// check if node is target node. If so, done!
+				if (Math.abs(node.x - destinationX) <  minObstacleSize && 
+					Math.abs(node.y - destinationY) <  minObstacleSize) {
+					resultNode = node;
+					break;
+				}
+			}	
+		}
+
+		if (resultNode != null) {
+			break;
+		}
+	}
+
+	// If the resultNod exists, backtrack and create a path
+	if (resultNode != null) {
+		var direction;
+		path.unshift(resultNode);
+
+		while (resultNode.parent != null) {
+			resultNode = resultNode.parent;
+			path.unshift(resultNode);
+		}
+	}
+
+	return path;
+}
